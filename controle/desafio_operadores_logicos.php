d25ad3078c0a61a492221df270d45b4755a9538e
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-05 10:21:20 -0300

    just for the science :white_check_mark:

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -1,7 +1,7 @@
 <div class="titulo">Desafio Operadores Lógicos</div>
 
 <!--
-    Dois trabalhos -> terça e quinta!
+    Dois trabalhos -> terça e quinta!!
     - Se os dois forem executados -> TV 50' e Sorvete
     - Se apenas um for executado -> TV 32' e Sorvete
     - Se nenhum for executado -> Fica em casa mais saudável!
