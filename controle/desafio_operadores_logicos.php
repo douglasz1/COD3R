8202b4aa5e38b48c63548b182b5e6b20ab7116f5
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-06 15:35:01 -0300

    :white_check_list:

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -1,11 +1,7 @@
 <div class="titulo">Desafio Operadores Lógicos</div>
 
 <!--
-<<<<<<< HEAD
     Dois trabalhos -> terça e quinta!
-=======
-    Dois trabalhos -> terça e quinta!!
->>>>>>> refs/remotes/origin/main
     - Se os dois forem executados -> TV 50' e Sorvete
     - Se apenas um for executado -> TV 32' e Sorvete
     - Se nenhum for executado -> Fica em casa mais saudável!
