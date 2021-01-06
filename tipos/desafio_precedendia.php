972ffc2c4795f9bfab360eece4d3a9f88dfbc70f
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-06 09:35:56 -0300

    :tada:

--- tipos/desafio_precedendia.php
+++ tipos/desafio_precedendia.php
@@ -2,7 +2,7 @@
 
 <?php
 echo '<p>1. Qual o valor será impresso?</p>';
-echo 2 + 5 * 3 + (12 / 6) / (-9 + 2 ** 3);
+echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3);
 
 echo '<p>2. Qual expressão imprime o valor 100?</p>';
 echo 'a) ', (1 + 2) * 20 - 15;
