c54ba3380b7e61fb642566b03fd9fff47796b05f
douglasz1 <douglas.santana.dev@gmail.com>
2021-02-03 08:49:40 -0300

    :fire:

--- menu.php
+++ menu.php
@@ -1,5 +1,79 @@
 <nav class="modulos">
-<<<<<<< HEAD
+    <div class="modulo laranja">
+        <h3>15. BD</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=db&file=criar_banco">
+                    Criar Banco
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=criar_tabela">
+                    Criar Tabela
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=inserir_1">
+                    Inserir Registro #01
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=consultar">
+                    Consultar Registros
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=excluir_1">
+                    Excluir Registro #01
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=excluir_2">
+                    Excluir Registro #02
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=inserir_2">
+                    Inserir Registro #02
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=alterar">
+                    Alterar Registro
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=inserir_pdo">
+                    PDO: Inserir
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=consultar_pdo">
+                    PDO: Consultar
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=excluir_pdo">
+                    PDO: Excluir
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=alterar_pdo">
+                    PDO: Alterar
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo roxo">
+        <h3>14. Formulário</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=formulario&file=formulario">
+                    Formulário
+                </a>
+            </li>
+        </ul>
+    </div>
     <div class="modulo azul">
         <h3>13. API</h3>
         <ul>
@@ -40,8 +114,6 @@
             </li>
         </ul>
     </div>
-=======
->>>>>>> 1f2dd5e4090e249aa2f871b80ce5938fddb45c25
     <div class="modulo vermelho">
         <h3>12. Sessão</h3>
         <ul>
